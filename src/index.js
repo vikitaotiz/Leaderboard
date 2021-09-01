c60eda9b457f4c63cc8d2d446cd6fac7b0d2f12b
Victor Otieno <victorotieno598@gmail.com>
2021-09-01 09:52:42 +0300

    Add function to add scores

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.css';
 import displayList from './displayList.js';
+import addScore from './addScore.js';
 
 const scoresList = document.querySelector('#scoresList');
+const scoresName = document.querySelector('#name');
+const scoresNumber = document.querySelector('#score');
+const submitBtn = document.querySelector('#submitBtn');
 
 const scores = [
   { name: 'Vikita', score: 30 },
@@ -12,3 +16,18 @@ const scores = [
 ];
 
 displayList(scoresList, scores);
+
+submitBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  const scoreData = {
+    name: scoresName.value,
+    score: scoresNumber.value,
+  };
+
+  // console.log(scoreData);
+  addScore(scores, scoreData);
+  scoresList.innerHTML = '';
+  displayList(scoresList, scores);
+  scoresName.value = '';
+  scoresNumber.value = '';
+});
